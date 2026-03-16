6346858bbdfcc447a4558cb7fd67d8b0b40bb046
Anjali Deshmukh <anjalideshmukh2541@gmail.com>
2026-03-17 02:52:30 +0530

    changes

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kr2n72ute7o"
+    "revision": "0.noumend5tlo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
